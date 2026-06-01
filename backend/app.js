40b046abfc52fc31665f66614da48841a34a4b3d
Geraldine-Edwards <geraldine.edwards25@gmail.com>
2026-06-01 23:16:58 +0100

    Remove console log for server startup with allowed origins

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ import express from "express";
 import { quotes } from './quotes.js'
 import { allowedOrigins } from './config.js';
 
-console.error('Starting server, allowed origins=', allowedOrigins)
 const app = express();
 const port = process.env.PORT || 3000;
 
